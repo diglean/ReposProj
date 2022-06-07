e3a29038658ff7b49184598d491b77df2603c621
diglean <diegoleandro2002@gmail.com>
2022-06-07 13:53:33 -0300

    Searching repos pt.2

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,5 +1,5 @@
 import React, { useState, useCallback } from 'react';
-import { FaGithub, FaPlus } from 'react-icons/fa'
+import { FaGithub, FaPlus, FaSpinner } from 'react-icons/fa'
 import { Container, Form, SubmitButton } from './styles'
 
 import api from '../../services/api';
@@ -8,20 +8,30 @@ export default function Main() {
 
     const [newRepo, setNewRepo] = useState('');
     const [repositorios, setRepositorios] = useState([]);
+    const [loading, setLoading] = useState(false);
 
 
     const handleSubmit = useCallback((e)=>{
         e.preventDefault();
 
         async function submit(){
-            const response = await api.get(`repos/${newRepo}`);
-    
-            const data = {
-                name: response.data.full_name,
+            setLoading(true);
+            try{
+
+                const response = await api.get(`repos/${newRepo}`);
+        
+                const data = {
+                    name: response.data.full_name,
+                }
+        
+                setRepositorios([...repositorios, data]);
+                setNewRepo('');
+
+            }catch(error){
+                console.log(error);
+            }finally{
+                setLoading(false);
             }
-    
-            setRepositorios([...repositorios, data]);
-            setNewRepo('');
         }
 
         submit();
@@ -47,8 +57,12 @@ export default function Main() {
                 onChange={handleInputChange}
                 />
 
-                <SubmitButton >
-                    <FaPlus color="#FFF" size={14} />
+                <SubmitButton loading={loading ? 1 : 0}>
+                    {loading ? (
+                        <FaSpinner color="#FFF" size={14}/>
+                    ) : (
+                        <FaPlus color="#FFF" size={14} />
+                    )}
                 </SubmitButton>
             </Form>
 
