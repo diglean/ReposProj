873d21c5fdb7d0d1550702f11d73b2c7575ab8f0
diglean <diegoleandro2002@gmail.com>
2022-06-07 12:53:57 -0300

    Styled component

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
-export default function Main(){
-    return(
-        <h1>Main</h1>
+import { Title } from './styles'
+
+export default function Main() {
+    return (
+        <Title>
+            Main
+        </Title>
     )
 }
