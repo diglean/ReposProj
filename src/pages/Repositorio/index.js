430ff369a98533fddba7be88b18558b52c9d31f9
diglean <diegoleandro2002@gmail.com>
2022-06-07 14:56:47 -0300

    Repos details

--- src/pages/Repositorio/index.js
+++ src/pages/Repositorio/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 
-export default function Repositorio(){
-    return(
-        <h1>Repositorio</h1>
+export default function Repositorio({match}) {
+    return (
+        <h1 style={{ color: '#FFF' }}>
+            {decodeURIComponent(match.params.repositorio)}
+        </h1>
     )
 }
